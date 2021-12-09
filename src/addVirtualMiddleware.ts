98136fa834b6460654b77fe0e0e9062717238dc3
TenshiGoan <angelgoan@gmail.com>
2021-12-10 02:00:05 +0300

    0.1.6

--- src/addVirtualMiddleware.ts
+++ src/addVirtualMiddleware.ts
@@ -1,6 +1,6 @@
-import { useNuxt } from "@nuxt/kit-edge";
+import { useNuxt, resolveAlias } from "@nuxt/kit-edge";
 import { resolve, normalize } from "pathe";
-import { createUnplugin } from "unplugin";
+import { rollupVirtual } from "./plugins/virtual";
 import { addNitroPlugin } from "./addNitroPlugin";
 import { randomUUID } from "crypto";
 import FS from "fs";
@@ -8,66 +8,38 @@ import { useNitroRollupBefore } from "./useNitroRollupBefore";
 
 type VirtualMiddleware = {
   src?: string;
+  route?: string;
+  lazy?: boolean;
   getContents?: (source: string) => string;
 };
 
 export function addVirtualMiddleware(opts: VirtualMiddleware) {
   const nuxt = useNuxt();
-  const plugin = createPlugin();
+  const plugin = rollupVirtual();
 
-  const name = `virtual.middleware.${randomUUID()}.ts`;
+  const name = `tgnu.virtual.middleware.${randomUUID()}.ts`;
   const alias = `#build/${name}`;
   const dst = resolve(nuxt.options.buildDir, name);
 
-  let rollup_context: any = {};
-
-  useNitroRollupBefore((context) => {
-    rollup_context = context;
+  useNitroRollupBefore((rollup_context) => {
     rollup_context.middleware.push({
-      route: "/",
+      route: opts.route ?? "/",
+      lazy: opts.lazy,
       handle: alias,
     });
     rollup_context.alias[alias] = dst;
   });
 
-  nuxt.options.alias[alias] = dst;
-  nuxt.options.build.transpile.push(dst);
-
-  nuxt.hook("build:done", () => {});
-
   addNitroPlugin(
-    () =>
-      plugin.rollup({
-        name: dst,
-        text: () => {
-          const source = FS.readFileSync(opts.src).toString();
-          return opts.getContents ? opts.getContents(source ?? "") : source;
-        },
-      }),
+    plugin({
+      name: dst,
+      text: () => {
+        const source = opts.src ? FS.readFileSync(opts.src).toString() : "";
+        return opts.getContents ? opts.getContents(source ?? "") : source;
+      },
+    }),
     true
   );
 
   return { name, alias, dst };
-}
-
-function createPlugin() {
-  return createUnplugin<{
-    name: string;
-    text: () => Promise<string> | string;
-  }>(({ name, text }) => {
-    const filter = (id: string) => normalize(id) === normalize(name);
-    return {
-      name: "@tgnu/utils/addVirtualMiddleware",
-      async load(id) {
-        if (id.startsWith("file:///")) {
-          id = id.slice(8);
-        }
-        if (filter(id)) {
-          return text();
-        } else {
-          return null;
-        }
-      },
-    };
-  });
 }
